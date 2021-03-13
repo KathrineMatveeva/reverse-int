01462779a7095275f09c3e6e440304939e094b62
Kate <kate.matveeva@gmail.com>
2021-03-13 18:29:55 +0300

    feat: add implementation reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
+ module.exports = function reverse (n) {
+    if (n < 0){
+        n = -n;
+    }
+
+    let str=n.toString();
+    let result = str.split('').reverse().join('');
+    
+    return Number(result)
   
 }
+
+
+
